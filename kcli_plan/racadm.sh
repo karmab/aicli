825b499203f80a441316950ff8b2de6dc424f96c
karmab <karimboumedhel@gmail.com>
2020-09-14 11:31:08 +0200

    deploy racadm within plan

---
+++ kcli_plan/racadm.sh
@@ -0,0 +1,6 @@
+echo """[racadm]
+name=Racadm
+baseurl=http://linux.dell.com/repo/hardware/dsu/os_dependent/RHEL8_64
+enabled=1
+gpgcheck=0""" > /etc/yum.repos.d/racadm.repo
+yum -y install openssl-devel srvadmin-idracadm7
