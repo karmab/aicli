bf336e5488035f937dc2f5b445c3e7afad932147
karmab <karimboumedhel@gmail.com>
2020-12-11 13:35:19 +0100

    support onprem deployment method

--- kcli_plan/install.sh
+++ kcli_plan/install.sh
@@ -1,6 +1,6 @@
 export HOME=/root
 cd /root
-dnf -y install python3-pip socat make
+dnf -y install python3-pip socat make tmux git
 pip3 install waiting
 git clone https://github.com/openshift/assisted-service
 cd assisted-service
@@ -8,18 +8,26 @@ IP=$(hostname -I | cut -d' ' -f1)
 REVERSE_NAME=$(dig -x $IP +short | sed 's/\.[^\.]*$//')
 AI_URL=http://${REVERSE_NAME:-$IP}:8090
 sed -i "s@REPLACE_BASE_URL@$AI_URL@" deploy/assisted-service-configmap.yaml
-make deploy-all {{ "TARGET=oc-ingress" if not minikube else "" }}
-make deploy-ui {{ "TARGET=oc-ingress" if not minikube else "" }}
-#docker run -v $PWD:/here --rm quay.io/ocpmetal/assisted-service:latest cp -r /clients/assisted-service-client-1.0.0.tar.gz /here
-docker build -t ocpmetal/assisted-service -f Dockerfile.assisted-service .
-docker run -v $PWD:/here --rm ocpmetal/assisted-service:latest cp -r /clients/assisted-service-client-1.0.0.tar.gz /here
-tar zxvf assisted-service-client-1.0.0.tar.gz
-cd assisted-service-client-1.0.0
-python3 setup.py install
-pip3 install git+https://github.com/karmab/assisted-installer-cli.git#egg=assisted-installer-cli
+{% if method == "minikube" %}
+make deploy-all
+make deploy-ui
 kubectl wait -n assisted-installer $(kubectl get pod -n assisted-installer -l app=assisted-service -o name) --for=condition=Ready
 tmux new-session -s port-forward-service -d "while true; do kubectl -n assisted-installer port-forward --address 0.0.0.0 svc/assisted-service 8090:8090; done"
 kubectl wait -n assisted-installer $(kubectl get pod -n assisted-installer -l app=ocp-metal-ui -o name) --for=condition=Ready
 tmux new-session -s port-forward-ui -d "while true; do kubectl -n assisted-installer port-forward --address 0.0.0.0 svc/ocp-metal-ui 8080:80; done"
+{% elif method == "openshift" %}
+export KUBECONFIG=/root/kubeconfig
+make deploy-all TARGET=oc-ingress
+make deploy-ui TARGET=oc-ingress
+{% else %}
+dnf -y module disable container-tools
+dnf -y install 'dnf-command(copr)'
+dnf -y copr enable rhcontainerbot/container-selinux
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_8/devel:kubic:libcontainers:stable.repo
+dnf -y install podman
+sed -i "s@SERVICE_BASE_URL=.*@SERVICE_BASE_URL=$AI_URL@" onprem-environment
+sed -i "s/5432,8000,8090,8080/5432:5432 -p 8000:8000 -p 8090:8090 -p 8080:8080/" Makefile
+make deploy-onprem
+{% endif %}
 echo "export AI_URL=$AI_URL" >> /root/.bashrc
 echo "export PATH=/usr/local/bin:\$PATH" >> /root/.bashrc
--- kcli_plan/minikube.sh
+++ kcli_plan/minikube.sh
@@ -1,3 +1,6 @@
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce conntrack iptables --nobest
+systemctl enable --now docker
 curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl > /usr/bin/kubectl
 chmod +x /usr/bin/kubectl
 export HOME=/root
