1edb23203733d7142ddead96baaadf3b109ca6c5
karmab <karimboumedhel@gmail.com>
2021-01-19 20:53:46 +0100

    skip update cluster when starting

--- ailib/__init__.py
+++ ailib/__init__.py
@@ -304,11 +304,16 @@ class AssistedClient(object):
         cluster_id = self.get_cluster_id(name)
         if '-day2' in name:
             base_name = name.replace('-day2', '')
-            base_cluster_id = self.get_cluster_id(base_name)
-            base_cluster = self.client.get_cluster(cluster_id=base_cluster_id)
-            cluster_update_params = {'api_vip': base_cluster.api_vip, 'base_dns_domain': base_cluster.base_dns_domain}
-            cluster_update_params = models.ClusterUpdateParams(**cluster_update_params)
-            self.client.update_cluster(cluster_id=cluster_id, cluster_update_params=cluster_update_params)
+            try:
+                base_cluster_id = self.get_cluster_id(base_name)
+                base_cluster = self.client.get_cluster(cluster_id=base_cluster_id)
+                cluster_update_params = {'api_vip': base_cluster.api_vip,
+                                         'base_dns_domain': base_cluster.base_dns_domain}
+                cluster_update_params = models.ClusterUpdateParams(**cluster_update_params)
+                self.client.update_cluster(cluster_id=cluster_id, cluster_update_params=cluster_update_params)
+            except:
+                warning("Not updating day2 cluster as base cluster was not found")
+                pass
             self.client.install_hosts(cluster_id=cluster_id)
         else:
             self.client.install_cluster(cluster_id=cluster_id)
