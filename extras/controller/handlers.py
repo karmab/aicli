68483fc8e773d6cfaf1d80395953e2eb2ca1fd6b
karmab <karimboumedhel@gmail.com>
2022-12-05 10:51:41 +0100

    aicli-controller: handle deletion

--- extras/controller/handlers.py
+++ extras/controller/handlers.py
@@ -37,3 +37,23 @@ def create_deployment(meta, spec, status, namespace, logger, **kwargs):
     kubeconfig = open(f"kubeconfig.{name}").read()
     kubeconfig = base64.b64encode(kubeconfig.encode()).decode("UTF-8")
     return {'result': 'success', 'kubeconfig': kubeconfig}
+
+
+@kopf.on.delete(DOMAIN, VERSION, 'aiclideployment')
+def delete_deployment(meta, spec, status, namespace, logger, **kwargs):
+    name = meta.get('name')
+    pprint(f"Handling create on deployment {name}")
+    url = os.environ.get('URL', 'https://api.openshift.com')
+    OFFLINETOKEN = os.environ.get('OFFLINETOKEN')
+    ai = AssistedClient(url, offlinetoken=OFFLINETOKEN)
+    for clu in ai.list_clusters():
+        if clu['name'] == name:
+            ai.delete_cluster(name)
+            break
+    for infra_env in ai.list_infra_envs():
+        infra_env_name = infra_env.get('name')
+        associated_infra_envs = [f"{name}_infra-env", f"{name}-day2_infra-env"]
+        if infra_env_name is not None and infra_env_name in associated_infra_envs:
+            infra_env_id = infra_env['id']
+            ai.delete_infra_env(infra_env_id)
+    return {'result': 'success'}
